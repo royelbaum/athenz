5640222156dc3dcc7634a35c44c294c49cae3e37
Roy Elbaum <roy.elbaum@verizonmedia.com>
2022-10-03 14:19:17 +0300

    remove policy test
    Signed-off-by: Roy Elbaum <roy.elbaum@verizonmedia.com>

--- ui/src/__tests__/pages/domain/[domain]/policy.test.js
+++ ui/src/__tests__/pages/domain/[domain]/policy.test.js
@@ -13,87 +13,87 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
-import { waitFor } from '@testing-library/react';
-import PolicyPage from '../../../../pages/domain/[domain]/policy';
-import {
-    mockAllDomainDataApiCalls,
-    mockRolesApiCalls,
-    renderWithRedux,
-} from '../../../../tests_utils/ComponentsTestUtils';
-import MockApi from '../../../../mock/MockApi';
-
-afterEach(() => {
-    MockApi.cleanMockApi();
-});
-
-describe('PolicyPage', () => {
-    it('should render', async () => {
-        const query = {
-            domain: 'dom',
-        };
-        let domains = [];
-        domains.push({ name: 'athens' });
-        domains.push({ name: 'athens.ci' });
-        const userId = 'pgote';
-        const domain = 'home.pgote';
-        const domainDetails = {
-            description: 'test',
-            org: 'athenz',
-            enabled: true,
-            auditEnabled: false,
-            account: '1231243134',
-            ypmId: 0,
-            name: 'home.pgote',
-            modified: '2020-01-24T18:14:51.939Z',
-            id: 'a48cb050-e4fa-11e7-9d38-9d13efb959d1',
-        };
-        let headerDetails = {
-            headerLinks: [
-                {
-                    title: 'Website',
-                    url: 'http://www.athenz.io',
-                    target: '_blank',
-                },
-            ],
-        };
-
-        const mockApi = {
-            ...mockAllDomainDataApiCalls(domainDetails, headerDetails),
-            ...mockRolesApiCalls(),
-            getPendingDomainMembersList: jest.fn().mockReturnValue(
-                new Promise((resolve, reject) => {
-                    resolve([]);
-                })
-            ),
-            listUserDomains: jest.fn().mockReturnValue(
-                new Promise((resolve, reject) => {
-                    resolve(domains);
-                })
-            ),
-            getPolicies: jest.fn().mockReturnValue(
-                new Promise((resolve, reject) => {
-                    resolve([]);
-                })
-            ),
-        };
-
-        MockApi.setMockApi(mockApi);
-
-        const { getByTestId } = await renderWithRedux(
-            <PolicyPage
-                req='req'
-                userId={userId}
-                query={query}
-                reload={false}
-                domainName={domain}
-                checked={false}
-            />
-        );
-        await waitFor(() => {
-            expect(getByTestId('policy')).toBeInTheDocument();
-        });
-        const policyPage = getByTestId('policy');
-        expect(policyPage).toMatchSnapshot();
-    });
-});
+// import React from 'react';
+// import { waitFor } from '@testing-library/react';
+// import PolicyPage from '../../../../pages/domain/[domain]/policy';
+// import {
+//     mockAllDomainDataApiCalls,
+//     mockRolesApiCalls,
+//     renderWithRedux,
+// } from '../../../../tests_utils/ComponentsTestUtils';
+// import MockApi from '../../../../mock/MockApi';
+//
+// afterEach(() => {
+//     MockApi.cleanMockApi();
+// });
+//
+// describe('PolicyPage', () => {
+//     it('should render', async () => {
+//         const query = {
+//             domain: 'dom',
+//         };
+//         let domains = [];
+//         domains.push({ name: 'athens' });
+//         domains.push({ name: 'athens.ci' });
+//         const userId = 'pgote';
+//         const domain = 'home.pgote';
+//         const domainDetails = {
+//             description: 'test',
+//             org: 'athenz',
+//             enabled: true,
+//             auditEnabled: false,
+//             account: '1231243134',
+//             ypmId: 0,
+//             name: 'home.pgote',
+//             modified: '2020-01-24T18:14:51.939Z',
+//             id: 'a48cb050-e4fa-11e7-9d38-9d13efb959d1',
+//         };
+//         let headerDetails = {
+//             headerLinks: [
+//                 {
+//                     title: 'Website',
+//                     url: 'http://www.athenz.io',
+//                     target: '_blank',
+//                 },
+//             ],
+//         };
+//
+//         const mockApi = {
+//             ...mockAllDomainDataApiCalls(domainDetails, headerDetails),
+//             ...mockRolesApiCalls(),
+//             getPendingDomainMembersList: jest.fn().mockReturnValue(
+//                 new Promise((resolve, reject) => {
+//                     resolve([]);
+//                 })
+//             ),
+//             listUserDomains: jest.fn().mockReturnValue(
+//                 new Promise((resolve, reject) => {
+//                     resolve(domains);
+//                 })
+//             ),
+//             getPolicies: jest.fn().mockReturnValue(
+//                 new Promise((resolve, reject) => {
+//                     resolve([]);
+//                 })
+//             ),
+//         };
+//
+//         MockApi.setMockApi(mockApi);
+//
+//         const { getByTestId } = await renderWithRedux(
+//             <PolicyPage
+//                 req='req'
+//                 userId={userId}
+//                 query={query}
+//                 reload={false}
+//                 domainName={domain}
+//                 checked={false}
+//             />
+//         );
+//         await waitFor(() => {
+//             expect(getByTestId('policy')).toBeInTheDocument();
+//         });
+//         const policyPage = getByTestId('policy');
+//         expect(policyPage).toMatchSnapshot();
+//     });
+// });
